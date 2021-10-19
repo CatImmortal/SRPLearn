0f1113e8072c156c31f4cc1fd1541a9ff0125578
CatImmortal <1304085906@qq.com>
2021-10-20 01:00:28 +0800

    学习ShaderGUI扩展

---
+++ Assets/CustomRP/Editor/CustomShaderGUI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor;
+public class CustomShaderGUI : ShaderGUI
+{
+    private MaterialEditor matEditor;
+    private Object[] mats;
+    private MaterialProperty[] matProps;
+
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        base.OnGUI(materialEditor, properties);
+
+        matEditor = materialEditor;
+        mats = matEditor.targets;
+        matProps = properties;
+    }
+
+    /// <summary>
+    /// 设置材质属性
+    /// </summary>
+    private void SetProp(string name,float value)
+    {
+        FindProperty(name, matProps).floatValue = value;
+    }
+
+    /// <summary>
+    /// 设置材质属性
+    /// </summary>
+    private void SetProp(string name,string keyword, bool value)
+    {
+        SetProp(name, value ? 1f : 0f);
+        SetKeyword(keyword, value);
+    }
+
+    /// <summary>
+    /// 设置关键字
+    /// </summary>
+    private void SetKeyword(string keyword,bool enabled)
+    {
+        foreach (Material mat in mats)
+        {
+            if (enabled)
+            {
+                mat.EnableKeyword(keyword);
+            }
+            else
+            {
+                mat.DisableKeyword(keyword);
+            }
+        }
+    }
+}
--- Assets/CustomRP/Shaders/Lit.shader
+++ Assets/CustomRP/Shaders/Lit.shader
@@ -64,4 +64,6 @@
            
         }
     }
+
+    CustomEditor "CustomShaderGUI"
 }
